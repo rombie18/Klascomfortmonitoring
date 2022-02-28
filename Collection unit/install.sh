3f1d0bd3e591d13a65e3298a045c271d9311ba79
Wout Rombouts <woutrombouts18@gmail.com>
2022-02-28 18:53:29 +0100

    Update install.sh

--- Collection unit/install.sh
+++ Collection unit/install.sh
@@ -1,22 +1,22 @@
 ### Install Docker ###
-sudo chmod +x ./.install/docker-install.sh
-sudo source ./.install/docker-install.sh
+sudo chmod +x ./.install/docker-install.sh && \
+sudo source ./.install/docker-install.sh && \
 
 ### Configure Kiosk ###
-sudo cp ./.install/autostart ~/.config/lxsession/LXDE-pi/autostart
-sudo cp ./.install/start-kiosk.sh ~/start-kiosk.sh
-sudo chmod +x ~/start-kiosk.sh
+sudo cp ./.install/autostart ~/.config/lxsession/LXDE-pi/autostart && \
+sudo cp ./.install/start-kiosk.sh ~/start-kiosk.sh && \
+sudo chmod +x ~/start-kiosk.sh && \
 
 ### Configure Screensaver ###
-sudo chmod +x ./.install/configure-screensaver.sh
-sudo source ./.install/configure-screensaver.sh
+sudo chmod +x ./.install/configure-screensaver.sh && \
+sudo source ./.install/configure-screensaver.sh && \
 
 ### Moving files to the right directory ###
-sudo cp ./.install/docker-compose.yaml ~/docker-compose.yaml
-sudo mv ./install.sh ./.install/install.sh
+sudo cp ./.install/docker-compose.yaml ~/docker-compose.yamlv
+sudo mv ./install.sh ./.install/install.sh && \
 
 ### Start Docker containers ###
-sudo docker-compose up -d
+sudo docker-compose up -d && \
 
 ### Reboot ###
-sudo reboot now
+sudo reboot now && \
